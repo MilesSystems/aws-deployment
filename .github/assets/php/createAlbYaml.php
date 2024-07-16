4c167dd5bbf9f05f6dadb8d403f940161aa0c49a
richardtmiles <richard@miles.systems>
2024-07-16 16:44:32 -0600

    idk whats going on

--- .github/assets/php/createAlbYaml.php
+++ .github/assets/php/createAlbYaml.php
@@ -8,7 +8,7 @@ $hasCert = !empty($certificateArns);
 
 $certificateParameters = $hasCert ? <<<EOF
   CertificateArns:
-    Type: List<AWS::ElasticLoadBalancingV2::ListenerCertificate>
+    Type: CommaDelimitedList
     Description: List of ACM certificates to be used by the load balancer listener
     Default: "$certificateArns"
 EOF : <<<EOF
