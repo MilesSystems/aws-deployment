a0671a09efd3ca0dbc7a1816d7885d9edba6600c
Richard T. Miles <Richard@miles.systems>
2025-04-05 19:32:40 -0600

    initial runs help solve oddities

--- .github/assets/shell/startImagePipelineExecution.sh
+++ .github/assets/shell/startImagePipelineExecution.sh
@@ -1,148 +1,118 @@
 #!/bin/bash
 
-set -eEBx
+set -eEuo pipefail
 
 ENVIRONMENT="$1"
 REPOSITORY_NICENAME="$2"
 IMAGES_TO_SAVE="$3"
-NeedImageRebuild="${4}"
-ImageBuilderForceRebuild="${5}"
+NeedImageRebuild="${4:-false}"
+ImageBuilderForceRebuild="${5:-false}"
 
-PIPELINE_ARN=$(aws imagebuilder list-image-pipelines --output text --filter "name=name,values=imagebuilder-${ENVIRONMENT}-${REPOSITORY_NICENAME}" --query 'imagePipelineList[-1].arn')
+PIPELINE_ARN=$(aws imagebuilder list-image-pipelines \
+  --filter "name=name,values=imagebuilder-${ENVIRONMENT}-${REPOSITORY_NICENAME}" \
+  --query 'imagePipelineList[?name==`imagebuilder-'"${ENVIRONMENT}"'-'"${REPOSITORY_NICENAME}"'`].arn' \
+  --output text || true)
 
 echo "PIPELINE_ARN: $PIPELINE_ARN"
 
-if [[ "" == "$PIPELINE_ARN" || "None" == "$PIPELINE_ARN" ]]; then
+if [[ -z "$PIPELINE_ARN" || "$PIPELINE_ARN" == "None" ]]; then
   echo "No pipeline was found. Exiting."
   exit 3
 else
   echo "Pipeline was found."
 fi
 
-echo "pipeline_arn=$PIPELINE_ARN" >>$GITHUB_OUTPUT
-echo "pipeline_arn=$PIPELINE_ARN" >>DEPLOY.txt
+echo "pipeline_arn=$PIPELINE_ARN" >> $GITHUB_OUTPUT
+echo "pipeline_arn=$PIPELINE_ARN" >> DEPLOY.txt
 
 NEXT_PAGE=''
+ALL_IMAGE_SUMMARIES="[]"
 
-IMAGE_ARNS=''
-IMAGE_STATUSES=''
-
-IMAGE_BUILDER_IMAGES=$(aws imagebuilder list-image-pipeline-images --image-pipeline-arn "$PIPELINE_ARN" \
-  --filters "name=name,values=recipe-imagebuilder-${ENVIRONMENT}-${REPOSITORY_NICENAME}")
-
-# delete old images
+# Paginate through image lists
 while :; do
-
-  if [[ "" != "$NEXT_PAGE" ]]; then
-
-    IMAGE_BUILDER_IMAGES=$(aws imagebuilder list-image-pipeline-images --image-pipeline-arn "$PIPELINE_ARN" \
+  if [[ -n "$NEXT_PAGE" ]]; then
+    PAGE_DATA=$(aws imagebuilder list-image-pipeline-images --image-pipeline-arn "$PIPELINE_ARN" \
       --filters "name=name,values=recipe-imagebuilder-${ENVIRONMENT}-${REPOSITORY_NICENAME}" \
       --next-token "$NEXT_PAGE")
-
-  fi
-
-  echo "$IMAGE_BUILDER_IMAGES" | jq --color-output
-
-  IMAGE_ARNS="$IMAGE_ARNS $(echo "$IMAGE_BUILDER_IMAGES" | jq '.imageSummaryList[].arn' --raw-output)"
-  IMAGE_STATUSES="$IMAGE_STATUSES $(echo "$IMAGE_BUILDER_IMAGES" | jq '.imageSummaryList[].state.status' --raw-output)"
-
-  if [[ "$IMAGE_BUILDER_IMAGES" == *"nextToken"* ]]; then
-
-    NEXT_PAGE=$(echo "$IMAGE_BUILDER_IMAGES" | jq '.nextToken')
-
   else
-
-    break
-
+    PAGE_DATA=$(aws imagebuilder list-image-pipeline-images --image-pipeline-arn "$PIPELINE_ARN" \
+      --filters "name=name,values=recipe-imagebuilder-${ENVIRONMENT}-${REPOSITORY_NICENAME}")
   fi
+
+  echo "$PAGE_DATA" | jq --color-output
+
+  PAGE_SUMMARIES=$(echo "$PAGE_DATA" | jq '.imageSummaryList')
+  ALL_IMAGE_SUMMARIES=$(jq -s '.[0] + .[1]' <(echo "$ALL_IMAGE_SUMMARIES") <(echo "$PAGE_SUMMARIES"))
+
+  NEXT_PAGE=$(echo "$PAGE_DATA" | jq -r '.nextToken // empty')
+  [[ -z "$NEXT_PAGE" ]] && break
 
   echo "THE NEXT PAGE TOKEN ($NEXT_PAGE)"
-
 done
 
-# Print the table header
+IMAGE_ARNS=$(echo "$ALL_IMAGE_SUMMARIES" | jq -r '.[].arn')
+IMAGE_STATUSES=$(echo "$ALL_IMAGE_SUMMARIES" | jq -r '.[].state.status')
+
+# Print image summary
 printf "\n%-40s %-20s\n" "IMAGE ARN" "STATUS"
 printf "%-40s %-20s\n" "----------------------------------------" "--------------------"
 
-# Print the images and their statuses
-IFS=' ' read -r -a IMAGE_ARN_ARRAY <<< "$IMAGE_ARNS"
-IFS=' ' read -r -a IMAGE_STATUS_ARRAY <<< "$IMAGE_STATUSES"
+IFS=$'\n' read -r -d '' -a IMAGE_ARN_ARRAY < <(echo "$IMAGE_ARNS" && printf '\0')
+IFS=$'\n' read -r -d '' -a IMAGE_STATUS_ARRAY < <(echo "$IMAGE_STATUSES" && printf '\0')
 
 for ((i=0; i<${#IMAGE_ARN_ARRAY[@]}; i++)); do
   printf "%-40s %-20s\n" "${IMAGE_ARN_ARRAY[$i]}" "${IMAGE_STATUS_ARRAY[$i]}"
 done
 
-TOTAL_ARNS=$(echo "$IMAGE_ARNS" | wc -l | awk '{$1=$1};1')
+TOTAL_ARNS=${#IMAGE_ARN_ARRAY[@]}
+echo "TOTAL_ARNS = $TOTAL_ARNS"
 
 count=0
 
-echo "TOTAL_ARNS = $TOTAL_ARNS"
-
-# The last 10 results must be kept regardless of date
-# You must iterate to the last page to get the newest images.
-# If the newest image ended in failure you must run the pipeline
-
-for arn in $IMAGE_ARNS; do
-
-  [[ "" == "$arn" ]] && continue
+for arn in ${IMAGE_ARN_ARRAY[@]}; do
+  [[ -z "$arn" ]] && continue
 
   count=$((count + 1))
-
   [ ! $((count + IMAGES_TO_SAVE)) -lt $TOTAL_ARNS ] && break
 
+  echo "Inspecting image $arn"
+
   aws imagebuilder get-image --image-build-version-arn "$arn" | jq --color-output '.image.outputResources'
+  AMIS=$(aws imagebuilder get-image --image-build-version-arn "$arn" | jq -r '.image.outputResources.amis[].image')
 
-  AMIS=$(aws imagebuilder get-image --image-build-version-arn "$arn" | jq '.image.outputResources.amis[].image' --raw-output)
-
-  if [ "" = "$AMIS" ]; then
-
-    echo "No AMI(s) found!"
-
+  if [[ -z "$AMIS" ]]; then
+    echo "No AMIs found for image $arn"
   else
-
-    echo -e "Found the following AMIs \n${AMIS}\nWe will gather EBS storage ID for deletion as well."
-
+    echo -e "Found the following AMIs:\n$AMIS\nCleaning them up."
     while IFS= read -r AMI; do
-
       source ./.github/assets/shell/deleteAMI.sh "$AMI"
-
-    done <<<"$AMIS"
-
+    done <<< "$AMIS"
   fi
 
-  echo "$count ) aws imagebuilder delete-image --image-build-version-arn $arn"
-
-  # TODO - We need to make sure we have the last 10 successful builds..
-  aws imagebuilder delete-image --image-build-version-arn $arn || exit 8
-
-  IMAGE_ARNS="${IMAGE_ARNS[@]/$arn/}"
-
+  echo "$count ) Deleting image: $arn"
+  aws imagebuilder delete-image --image-build-version-arn "$arn" || echo "Failed to delete $arn"
 done
 
-LAST_BUILD_INFO=$(echo $IMAGE_BUILDER_IMAGES | jq '.imageSummaryList[-1]')
+# Get last image info if available
+LAST_BUILD_INFO=$(echo "$ALL_IMAGE_SUMMARIES" | jq '.[-1]')
 
-LAST_BUILD_ARN=$(echo $LAST_BUILD_INFO | jq '.arn' --raw-output)
+if [[ "$LAST_BUILD_INFO" == "null" || -z "$LAST_BUILD_INFO" ]]; then
+  echo "No previous images found."
+  LAST_BUILD_STATS="none"
+  LAST_BUILD_ARN=""
+else
+  LAST_BUILD_ARN=$(echo "$LAST_BUILD_INFO" | jq -r '.arn')
+  LAST_BUILD_STATS=$(echo "$LAST_BUILD_INFO" | jq -r '.state.status')
+  echo "The last build status was ($LAST_BUILD_STATS)"
+fi
 
-LAST_BUILD_STATS=$(echo $LAST_BUILD_INFO | jq '.state.status' --raw-output)
-
-echo "The last build status was ($LAST_BUILD_STATS)"
-
-# we should change this outputs.all nonsense to a bash regex comparison =~
-# @link https://stackoverflow.com/questions/17420994/how-can-i-match-a-string-with-a-regex-in-bash
-if [[ "true" == "$NeedImageRebuild" || "true" == "$ImageBuilderForceRebuild" ]] \
-  ; then
-
-  if [[ "\"BUILDING\"" == "$LAST_BUILD_STATS" ]]; then
-
+if [[ "$NeedImageRebuild" == "true" || "$ImageBuilderForceRebuild" == "true" ]]; then
+  if [[ "$LAST_BUILD_STATS" == "BUILDING" && -n "$LAST_BUILD_ARN" ]]; then
     echo "Canceling last image build ($LAST_BUILD_ARN)"
-
     aws imagebuilder cancel-image-creation --image-build-version-arn "$LAST_BUILD_ARN"
-
   fi
 
-  echo "Creating new image."
-
-  echo "PIPELINE_ARN=$PIPELINE_ARN"
+  echo "Creating new image for pipeline: $PIPELINE_ARN"
 
   BUILT_IMAGE_ARN=$(aws imagebuilder start-image-pipeline-execution \
     --image-pipeline-arn "$PIPELINE_ARN" \
@@ -153,22 +123,15 @@ if [[ "true" == "$NeedImageRebuild" || "true" == "$ImageBuilderForceRebuild" ]] 
 
   echo "image_rebuilt=1" >> $GITHUB_OUTPUT
   echo "image_rebuilt=1" >> IMAGE_BUILDER.txt
-
   echo "STARTED BUILT_IMAGE_ARN=$BUILT_IMAGE_ARN"
-
 else
-
-  echo "image_rebuilt=0" >> $GITHUB_OUTPUT
-  echo "image_rebuilt=0" >> IMAGE-BUILDER.txt
-
   echo "No changes detected :)"
-
-  # https://docs.aws.amazon.com/cli/latest/userguide/cli-usage-filter.html
   BUILT_IMAGE_ARN="$LAST_BUILD_ARN"
 
+  echo "image_rebuilt=0" >> $GITHUB_OUTPUT
+  echo "image_rebuilt=0" >> IMAGE_BUILDER.txt
   echo "USING ALREADY BUILT_IMAGE_ARN=$BUILT_IMAGE_ARN"
 fi
 
 echo "image_arn=$BUILT_IMAGE_ARN" >> $GITHUB_OUTPUT
-echo "image_arn=$BUILT_IMAGE_ARN" >>IMAGE-BUILDER.txt
-
+echo "image_arn=$BUILT_IMAGE_ARN" >> IMAGE_BUILDER.txt
